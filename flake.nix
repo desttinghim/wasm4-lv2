23c3938b67a391b5e63088d8b9697c97ffe6a2e0
Louis Pearson <opensource@louispearson.work>
2022-08-23 12:02:48 -0600

    Initial commit with flake.nix

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "WASM-4's APU in a LV2 plugin.";
+
+  inputs = {
+    zig.url = "github:arqv/zig-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+    unstable.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, zig, flake-utils, unstable }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        upkgs = unstable.legacyPackages.${system};
+      in
+      {
+
+        # nix develop
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            zig.packages.${system}.master.latest
+            pkgs.lv2
+            pkgs.lv2lint
+          ];
+        };
+
+      });
+}
