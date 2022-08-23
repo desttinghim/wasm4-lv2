0934c25879e7224ae2bd054c88021ace50935b5d
Louis Pearson <opensource@louispearson.work>
2022-08-23 16:09:25 -0600

    Add search and replace step

--- build.zig
+++ build.zig
@@ -1,11 +1,23 @@
 const std = @import("std");
+const ReplaceStep = @import("tools/Replace.zig");
 
 pub fn build(b: *std.build.Builder) void {
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
+    // TODO: make build use platform lib extension
+    const lib_ext = ".so";
+    const replace = ReplaceStep.create(b, .{
+        .source_path = .{ .path = "manifest.ttl.in" },
+        .output_name = "manifest.ttl",
+        .replacements = &[_]ReplaceStep.Replacement{
+            .{ .search = "@LIB_EXT@", .replacement = lib_ext },
+        },
+    });
+
     const lib = b.addSharedLibrary("amp", "src/main.zig", .unversioned);
+    lib.step.dependOn(&replace.step);
     lib.setBuildMode(mode);
     lib.linkLibC();
     lib.addIncludePath("deps/lv2/include");
---
+++ tools/Replace.zig
@@ -0,0 +1,72 @@
+//! Uses zig-ldtk to convert a ldtk file into a binary format for wired
+const std = @import("std");
+
+const KB = 1024;
+const MB = 1024 * KB;
+
+const ReplaceStep = @This();
+
+pub const Replacement = struct {
+    search: []const u8,
+    replacement: []const u8,
+};
+
+step: std.build.Step,
+builder: *std.build.Builder,
+source_path: std.build.FileSource,
+replacements: []const Replacement,
+output_name: []const u8,
+output: std.build.GeneratedFile,
+
+pub fn create(b: *std.build.Builder, opt: struct {
+    source_path: std.build.FileSource,
+    replacements: []const Replacement,
+    output_name: []const u8,
+}) *@This() {
+    var result = b.allocator.create(ReplaceStep) catch @panic("memory");
+    result.* = ReplaceStep{
+        .step = std.build.Step.init(.custom, "search and replace values in a file at build time", b.allocator, make),
+        .builder = b,
+        .source_path = opt.source_path,
+        .replacements = opt.replacements,
+        .output_name = opt.output_name,
+        .output = undefined,
+    };
+    result.*.output = std.build.GeneratedFile{ .step = &result.*.step };
+    return result;
+}
+
+fn make(step: *std.build.Step) !void {
+    const this = @fieldParentPtr(ReplaceStep, "step", step);
+
+    const allocator = this.builder.allocator;
+    const cwd = std.fs.cwd();
+
+    // Get path to source and output
+    const source_src = this.source_path.getPath(this.builder);
+    const output = this.builder.getInstallPath(.lib, this.output_name);
+
+    // Open file
+    const source_file = try cwd.openFile(source_src, .{});
+    defer source_file.close();
+    const source = try source_file.readToEndAlloc(allocator, 10 * MB);
+    defer allocator.free(source);
+
+
+    var replaced_data = try allocator.dupe(u8, source);
+    defer allocator.free(replaced_data);
+    for (this.replacements) |replacement| {
+        var new_data = try std.mem.replaceOwned(u8, allocator, replaced_data, replacement.search, replacement.replacement);
+        allocator.free(replaced_data);
+        replaced_data = new_data;
+    }
+
+    // Open output file and write data into it
+    cwd.makePath(this.builder.getInstallPath(.lib, "")) catch |e| switch (e) {
+        error.PathAlreadyExists => {},
+        else => return e,
+    };
+    try cwd.writeFile(output, replaced_data);
+
+    this.output.path = output;
+}
